c88bb7b66ae2beba3010c5d9bbeb40ca26fa1813
ChaClyde <carca@filamer.edu.ph>
2026-05-15 13:18:01 +0800

    Added Input and Properties

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,11 +1,29 @@
 import { Route, Routes } from "react-router-dom"
 import AppLayout from "../layout/AppLayout"
+import FloatingLabelInput from "../components/Input/FloatingLabelInput";
 
 
 const SampleComponent = () => {
     return (
         <>
-            <h1 className="text-red-600">Hello, World!</h1>
+            <h1 className="text-red-600 mb-4">Hello, World!</h1>
+            <div className="mb-4">
+                <FloatingLabelInput
+                    label="Role"
+                    type="text"
+                    name="Role"
+                    required
+                    autoFocus
+                />
+            </div>
+            <div className="mb-4">
+                <FloatingLabelInput
+                    label="Description"
+                    type="text"
+                    name="Description"
+                    required
+                />
+            </div>
         </>
     );
 };
